a27842ca41801e7e3da03d2cb1de560d726eff53
Hamza Bin Shahzad <hamzabinshahzad@live.com>
2023-06-04 23:50:46 +0500

    WIP

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using ModuleAssignment.Data;
 using ModuleAssignment.Interfaces;
@@ -8,6 +9,15 @@ using ModuleAssignment.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Authentication service configuration.
+builder.Services.AddAuthentication(auth =>
+{
+    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    auth.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
