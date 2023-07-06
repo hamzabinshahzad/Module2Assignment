4738fec856f2be25ebc6bad2f82217e92782a1f1
HamzaShahzadContour <hamzabinshahzad@live.com>
2023-07-06 21:01:48 +0500

    Added Filter for prevent numeric input.

---
+++ Filters/ValidationFilters/ForbidNumericFilter.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace ModuleAssignment.Filters.ValidationFilters
+{
+    public class ForbidNumericFilter : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (!Regex.Match(value.ToString(), @"^([^0-9]*)$").Success) return new ValidationResult("Cannot contain numeric digits!");
+            else return ValidationResult.Success;
+        }
+    }
+}
--- Models/EmployeeAddress.cs
+++ Models/EmployeeAddress.cs
@@ -1,4 +1,5 @@
-﻿using Swashbuckle.AspNetCore.Annotations;
+﻿using ModuleAssignment.Filters.ValidationFilters;
+using Swashbuckle.AspNetCore.Annotations;
 using System.ComponentModel.DataAnnotations;
 
 namespace ModuleAssignment.Models
@@ -14,17 +15,17 @@ namespace ModuleAssignment.Models
         public string SteetAddress { get; set; }
 
         [Required]
-        [RegularExpression(@"^([^0-9]*)$", ErrorMessage = "City Name cannot contain any numeric digits!")]
+        [ForbidNumericFilter]
         [StringLength(255, MinimumLength = 1, ErrorMessage = @"City name length must be between {1} and {2} characters long!")]
         public string City { get; set; }
 
         [Required]
-        [RegularExpression(@"^([^0-9]*)$", ErrorMessage = "State Name cannot contain any numeric digits!")]
+        [ForbidNumericFilter]
         [StringLength(255, MinimumLength = 1, ErrorMessage = @"State name length must be between {1} and {2} characters long!")]
         public string State { get; set; }
 
         [Required]
-        [RegularExpression(@"^([^0-9]*)$", ErrorMessage = "Country Name cannot contain any numeric digits!")]
+        [ForbidNumericFilter]
         [StringLength(255, MinimumLength = 1, ErrorMessage = @"Country name length must be between {1} and {2} characters long!")]
         public string Country { get; set; }
 
